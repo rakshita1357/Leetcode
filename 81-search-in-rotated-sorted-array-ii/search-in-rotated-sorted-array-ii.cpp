3e559aa649dcebbc6f9f53461b718df0fa873479
Rakshita <148205389+rakshita1357@users.noreply.github.com>
2025-11-02 21:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (55.26%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -6,8 +6,9 @@ public:
             mid=(low+high)/2;
             if(nums[mid]==target)
                 return true;
-            if(nums[low]==nums[mid]){
+            if(nums[low]==nums[mid]&&nums[mid]==nums[high]){
                 low++;
+                high--;
                 continue;
             }
             if(nums[low]<=nums[mid]){
